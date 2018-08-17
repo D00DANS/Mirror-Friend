aa87802a11f0109ad8a506f9d3cb0b93dd175aee
Aaron Lambiase <aaronlambiase4@gmail.com>
2018-08-16 18:54:07 -0700

    Update Commit
    Updated some minor things

--- Companion/src/main/java/com/example/mirrorfriend/mirrorfriend/Companion/MainActivity.java
+++ Companion/src/main/java/com/example/mirrorfriend/mirrorfriend/Companion/MainActivity.java
@@ -28,20 +28,21 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
+    public static final int MY_PERMISSIONS_REQUEST = 1;
     BluetoothAdapter mBluetoothAdapter;
-    Blue2 mBluetoothConnection;
+    Blue2 mBlue2;
 
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     BluetoothDevice mBTDevice;
 
-    String pLat, pLng, pTime, txtSize, tColor, bColor;
+    String pLat, pLng, txtSize, tColor, bColor;
 
     private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
+            assert action != null;
             if (action.equals(BluetoothDevice.ACTION_BOND_STATE_CHANGED)) {
                 BluetoothDevice mDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 if (mDevice.getBondState() == BluetoothDevice.BOND_BONDED) {
@@ -62,11 +63,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (checkBluetoothPermission() && checkBluetoothAdminPermission()) {
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            startConnection();
+        }
 
-        startConnection();
-
-        /*  Location section: Variable content to obtain - Latitude, Longitude, Time.   */
         if (checkLocationPermission()) {
             //Make an instance of LocationManager called lm(short for locationManager)
             LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -78,11 +79,10 @@ public class MainActivity extends AppCompatActivity {
             double phoneLng = lkl.getLongitude();
             pLat = Double.toString(phoneLat);
             pLng = Double.toString(phoneLng);
-        }/*  End Location Section.    */
+        }
 
         final int[] RR = {0};
 
-        /*  SeekBar Section: Variable content to obtain - refreshRate integer.  */
         //Make a SeekBar called refreshRate.
         SeekBar refreshRate = findViewById(R.id.seekBarRefreshRate);
         refreshRate.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -99,9 +99,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar refreshRate) {
             }
-        });/*  End SeekBar Section. */
+        });
 
-        /*  Switch Section: Variable content to obtain - textSizeValue Boolean.  */
         //Make a boolean called textSizeValue to hold the true or false value that will determine text size on mirror.
         final boolean[] textSizeValue = {false};
         Switch textSize = findViewById(R.id.switchTextSize);
@@ -115,8 +114,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        /*  End Switch Section.  */
-        /*  Text Color Spinner Section: Variable content to obtain - Text color string. */
+
         Spinner spinnerTextColor = findViewById(R.id.spinnerTextColor);
         //Create an instance of spinner that is connected to the spinnerTextColor.
         ArrayAdapter<CharSequence> adapterTextColor = ArrayAdapter.createFromResource(this,
@@ -129,9 +127,7 @@ public class MainActivity extends AppCompatActivity {
         int spinnerTextColorValue = (int) spinnerTextColor.getSelectedItem();
         tColor = Integer.toString(spinnerTextColorValue);
         // Record the selected item as an int to be sent to mirror.
-        /*  End Text Color Spinner Section. */
 
-        /*  Background Color Spinner Section: Variable content to obtain - Background color string. */
         Spinner spinnerBackgroundColor = findViewById(R.id.spinnerBackgroundColor);
         ArrayAdapter<CharSequence> adapterBackgroundColor = ArrayAdapter.createFromResource(this,
                 R.array.TextBackgroundColors, android.R.layout.simple_spinner_item);
@@ -140,7 +136,6 @@ public class MainActivity extends AppCompatActivity {
         int spinnerBackgroundColorValue = (int) spinnerBackgroundColor.getSelectedItem();
         bColor = Integer.toString(spinnerBackgroundColorValue);
         // See spinnerTextColor above and apply to second spinnerBackgroundColor.
-        /*  End Background Color Spinner Section. */
 
         Timer timer = new Timer();
         timer.schedule(new refresh(), 0, RR[0]);
@@ -151,12 +146,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startBTConnection(BluetoothDevice device, UUID uuid) {
-        mBluetoothConnection.startClient(device, uuid);
+        mBlue2.startClient(device, uuid);
     }
 
     public boolean checkLocationPermission() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean checkBluetoothPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission.BLUETOOTH}, MY_PERMISSIONS_REQUEST);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean checkBluetoothAdminPermission() {
+        if (ActivityCompat.checkSelfPermission(this, permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission.BLUETOOTH_ADMIN}, MY_PERMISSIONS_REQUEST);
             return false;
         } else {
             return true;
@@ -166,9 +179,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
         switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_LOCATION: {
+            case MY_PERMISSIONS_REQUEST: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     ContextCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION);
+                    ContextCompat.checkSelfPermission(this, permission.BLUETOOTH);
+                    ContextCompat.checkSelfPermission(this, permission.BLUETOOTH_ADMIN);
                 }
             }
         }
@@ -183,11 +198,11 @@ public class MainActivity extends AppCompatActivity {
             byte[] byteTC = tColor.getBytes(Charset.defaultCharset());
             byte[] byteBGC = bColor.getBytes(Charset.defaultCharset());
 
-            mBluetoothConnection.write(byteLat);
-            mBluetoothConnection.write(byteLng);
-            mBluetoothConnection.write(byteSize);
-            mBluetoothConnection.write(byteTC);
-            mBluetoothConnection.write(byteBGC);
+            mBlue2.write(byteLat);
+            mBlue2.write(byteLng);
+            mBlue2.write(byteSize);
+            mBlue2.write(byteTC);
+            mBlue2.write(byteBGC);
         }
     }
 
